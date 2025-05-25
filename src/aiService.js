4c0ec126107295ec596cd315154ed257dd977897
mulandi <mulandimorris1@gmail.com>
2025-05-25 17:51:23 +0300

    updated aiService to handle null api_key

--- src/aiService.js
+++ src/aiService.js
@@ -2,13 +2,23 @@ const { OpenAI } = require("openai");
 
 require("dotenv").config();
 
-// Initialize OpenAI API
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY, // Load API key from .env
-});
+let openai = null;
+const apiKey = process.env.OPENAI_API_KEY;
+
+// Initialize OpenAI API if the api is set
+if (apikey && apikey !== 'not_set') {
+  openai = new OpenAI({ apiKey });
+} else {
+  console.warn("OPENAI_API_KEY is not set. AI features are disabled.");
+}
 
 // Function to send a message to ChatGPT
 const askChatGPT = async (message) => {
+  if (!openai) {
+    return {
+      error: "AI features is currently disabled because the API key is missing.",
+    };
+  }
   try {
     const completion = await openai.chat.completions.create({
       model: "gpt-3.5-turbo", 
